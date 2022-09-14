cc3734be9594583f18a1619e10d69e29848c79f5
Charity Nelima <wa.nelimacharity@gmail.com>
2022-09-14 17:11:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split('').reverse().join('') === word ? true : false
 }
 
 /* 
   Add your pseudocode here
+  Reverse string
+  if the reversed string equal to the original string
+    return true
+  otherwise
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  To get the reversed string, split the string to array of letter, reverse the array of letter and then rejoin the letter to a string again
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,11 +22,8 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
